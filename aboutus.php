5cb92be84c923ee6fea92f0a1f2fcfa459619b53
Akash620347 <akash983530@gmail.com>
2023-07-18 11:23:45 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About-Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+     
+    </style>
+  </head>
+  <body>
+            <?php
+                include 'addon/_header.php';
+            ?>
+        <div class="container-fluid  " >
+           
+           <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime cumque eveniet perferendis quod, facere iure doloribus odit hic quas, excepturi suscipit est obcaecati officia delectus rem architecto, minima blanditiis optio saepe? Doloribus unde cumque enim dolorem, vero quos rerum accusamus culpa voluptatem. Incidunt quibusdam fugit, voluptates quos hic ex sint odio non, eum, facere excepturi deleniti perferendis animi nulla alias quae. Rerum ipsam fugiat amet, aut labore porro. Ducimus asperiores, explicabo necessitatibus, tempora illo eos error provident iste recusandae rerum animi quis aliquid aperiam nostrum quidem libero possimus? Esse enim quis molestias impedit, deserunt obcaecati quo? Natus, deserunt quasi.
+           </p>
+        </div>
+        <?php
+                include 'addon/_footer.php';
+          ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
